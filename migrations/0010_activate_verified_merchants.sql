0e1440dcce76317a3fb046655f29391b1c1f8cea
hnokim2-commits <h.nokim2@gmail.com>
2026-09-13 15:23:35 +0300

    Add files via upload

--- migrations/0010_activate_verified_merchants.sql
+++ migrations/0010_activate_verified_merchants.sql
@@ -1,5 +1,5 @@
 UPDATE merchants m
-SET verification='approved', is_accepting_orders=true, updated_at=now()
+SET verification='approved', is_accepting_orders=true
 WHERE (
   SELECT count(DISTINCT d.document_type)
   FROM merchant_documents d
